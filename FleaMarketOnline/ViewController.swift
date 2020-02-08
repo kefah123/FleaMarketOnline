072d7c0c4c21b169abb6c0991d0c3861822855a6
kefah123 <kefah@uvm.edu>
2020-02-08 15:26:38 -0500

    second commit

--- FleaMarketOnline/ViewController.swift
+++ FleaMarketOnline/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let ref = Database.database().reference();
+        ref.child("id/name").setValue("key")
     }
 
-
+   
 }
 

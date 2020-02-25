cfa20be9b53d8f1aa8b4c4494134df4f4247e131
rendong <rzhang6@uvm.edu>
2020-02-25 09:34:11 -0500

    Signup auth setup
    validate users, create an instance for a user, upddate pods: firesstore, core, auth, update system config

--- FleaMarketOnline/LoginViewController.swift
+++ FleaMarketOnline/LoginViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpElements()
         // Do any additional setup after loading the view.
     }
+    func setUpElements(){
+        errorLabel.alpha = 0
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passwordTextfield)
+        Utilities.styleFilledButton(loginButton)
+        
+    }
     
-
     /*
     // MARK: - Navigation
 
@@ -27,4 +39,6 @@ class LoginViewController: UIViewController {
     }
     */
 
+    @IBAction func loginTapped(_ sender: Any) {
+    }
 }
--- FleaMarketOnline/SignUpViewController.swift
+++ FleaMarketOnline/SignUpViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
-
+import FirebaseDatabase
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextfield: UITextField!
+    @IBOutlet weak var lastNameTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
+    func setUpElements(){
+        errorLabel.alpha = 0
+        Utilities.styleTextField(firstNameTextfield)
+        Utilities.styleTextField(lastNameTextfield)
+        Utilities.styleTextField(emailTextfield)
+        Utilities.styleTextField(passwordTextfield)
+        Utilities.styleFilledButton(signUpButton)
+        
+    }
     /*
     // MARK: - Navigation
 
@@ -26,5 +47,68 @@ class SignUpViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    //validation, check everything in the text field is in good types
+    //or it will return errors meesags
+    func validateFields() -> String?{
+        //check if all fields are filled
+        if (firstNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            lastNameTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextfield.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "")
+        {
+            return "Please fill in all fields"
+        }
+        //check if all password is secure
+        let cleanPw = passwordTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if Utilities.isPasswordValid(cleanPw) == false {
+            return "Please make sure your password be at least 8 characters with a special character and a number "
+        }
+        return nil
+    }
+    @IBAction func signUpTapped(_ sender: Any) {
+        let error = validateFields()
+        if error != nil{
+            //there is an error message will be shown
+            errorLabel.text = error!
+            errorLabel.alpha = 1
+        }else{
+            //clean up data, no white space and newlines
+            let firstName = firstNameTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextfield.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            //create a user
+            Auth.auth().createUser(withEmail: "test", password: "test") { (result, error) in
+                //if there is an error
+                if error != nil {
+                    self.errorLabel.text = "Cannot create user "
+                    self.errorLabel.alpha = 1
+                }else{
+                    //create an instance for new user
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(
+                        data: ["firstname":firstName,
+                               "lastname":lastName
+                               "email":email
+                               "password":password
+                            "id":result!.user.uid)
+                    {(error) in
+                            if error != nil{
+                                self.errorLabel.text = "Cannot create user in database "
+                                self.errorLabel.alpha = 1
+                            }
+                    }
+                }
+                
+            }
+            // transfer to next view
+            self.transitions()
+        }
+    }
+    
+    
+    func transitions(){
+        
+    }
+    
 }

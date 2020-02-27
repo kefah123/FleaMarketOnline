a4e424cf4fbf9d745f1a7243ea0b5239456b84e0
rendong <rzhang6@uvm.edu>
2020-02-27 10:49:05 -0500

    Update Main, create temp menu

---
+++ FleaMarketOnline/TempMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TempMenuViewController.swift
+//  FleaMarketOnline
+//
+//  Created by Rendong Zhang on 2/27/20.
+//  Copyright © 2020 HEWZ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TempMenuViewController: UIViewController {
+
+    @IBOutlet weak var LoginSignUpButton: UIButton!
+    
+    @IBOutlet weak var ProfileButton: UIButton!
+    @IBOutlet weak var CartButton: NSLayoutConstraint!
+    @IBOutlet weak var ChatButton: UIButton!
+    @IBOutlet weak var HomeButton: UIButton!
+}

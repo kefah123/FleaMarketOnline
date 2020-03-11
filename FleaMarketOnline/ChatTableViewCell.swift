fb89eda8ad13890d66dee4c15deca243f91b28de
kefah123 <kefah@uvm.edu>
2020-03-10 23:01:41 -0400

    chat interface updates

---
+++ FleaMarketOnline/ChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatTableViewCell.swift
+//  FleaMarketOnline
+//
+//  Created by Kwasi Efah on 3/5/20.
+//  Copyright © 2020 HEWZ. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

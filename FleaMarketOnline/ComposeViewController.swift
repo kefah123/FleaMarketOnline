741ca45f0449437234848f6b3aef95d73eefda95
rendong <rzhang6@uvm.edu>
2020-03-17 16:10:13 -0400

    home update 2

---
+++ FleaMarketOnline/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  FleaMarketOnline
+//
+//  Created by Rendong Zhang on 3/17/20.
+//  Copyright © 2020 HEWZ. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

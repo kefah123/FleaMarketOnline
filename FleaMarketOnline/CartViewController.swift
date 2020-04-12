ae3b08b6967fdb833bb03eb370be07270f386ae5
rendong <rzhang6@uvm.edu>
2020-04-12 19:52:37 -0400

    fixed cart bug

--- FleaMarketOnline/CartViewController.swift
+++ FleaMarketOnline/CartViewController.swift
@@ -15,32 +15,36 @@ import FirebaseFirestore
 import FirebaseDatabase
 
 class CartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
     var postData = [[String]]()
-    
     var ref: DatabaseReference?
     var databaseHandle:DatabaseHandle?
-    
-    @IBOutlet weak var tableView: UITableView!
+
+    @IBOutlet weak var cartTableView: UITableView!
     
     override func viewDidLoad() {
         print("cart test")
         //self.tableView.reloadData()
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        cartTableView.delegate = self
+        cartTableView.dataSource = self
         //Set the firebase reference
         ref = Database.database().reference()
         //Retrieve the posts and listen for changes
         databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
-            //code to execute when a chld is added user "Posts"
+            //code to execute when a child is added user "Posts"
             //take the value from the snapshot and added it to the post data array
             let post = snapshot.value as? [String]
+            print(post)
             if let actualPost = post{
-                //append the data to our postData array
+//                //append the data to our postData array
+                
                 self.postData.append(actualPost)
-                self.tableView.reloadData()
+                self.cartTableView.reloadData()
             }
-               })
+               }){ (error) in
+               print(error.localizedDescription)
+        }
         
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,15 +54,9 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
           let cell = tableView.dequeueReusableCell(withIdentifier: "CartCell", for: indexPath)
-          print(postData[indexPath.row])
+          print("cart items :",postData[indexPath.row])
           cell.textLabel?.text = postData[indexPath.row][0]
           return cell
       }
-      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-          let Storyboard = UIStoryboard(name: "Cart", bundle: nil)
-          let vc = Storyboard.instantiateViewController(withIdentifier: "HomeCellViewController") as! HomeCellViewController
-          vc.getName = postData[indexPath.row][0]
-          print(postData[indexPath.row][0])
-          self.navigationController?.pushViewController(vc, animated: true)
-      }
+
 }

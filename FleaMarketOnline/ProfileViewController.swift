8cc56dc9927b8748e7a68a1f323649f6f9ab0291
HengjunWU <hengjun.wu@uvm.edu>
2020-04-14 01:30:41 -0400

    added buttons that connected to database, and fetch current user information as textfield

--- FleaMarketOnline/ProfileViewController.swift
+++ FleaMarketOnline/ProfileViewController.swift
@@ -7,15 +7,43 @@
 //
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
-class ProfileViewController: UITabBarController {
+class ProfileViewController: UIViewController {
+        
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     
-    @IBOutlet weak var editButton: UIButton!
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        checkLoggedInUserStatus()
+        checkLoggedInUserStatus()
+//        ref = Database.database().reference()
+//        let uid = Auth.auth().currentUser?.uid
+//        ref!.child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+//          // Get user value
+//          let value = snapshot.value as? NSDictionary
+//          let name = value?["name"] as? String ?? ""
+//          let email = value?["email"] as? String ?? ""
+//            self.nameField.text = name
+//            self.emailField.text = email
+//
+//        })
     }
+    
+    @IBAction func nameEditingButton(_ sender: Any) {
+        let uid = Auth.auth().currentUser?.uid
+        self.ref!.child("users/\(String(describing: uid))/name").setValue(nameField.text)
+    }
+    
+    @IBAction func emailEditingButton(_ sender: Any) {
+        let uid = Auth.auth().currentUser?.uid
+        self.ref!.child("users/\(String(describing: uid))/email").setValue(emailField.text)
+    }
+    
+    
     fileprivate func checkLoggedInUserStatus(){
         
         if Auth.auth().currentUser == nil{
